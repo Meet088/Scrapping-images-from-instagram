d7dfb6e01296d167984d9c8473048f8f0786f3f1
seanrow <prekshyabasnet3@gmail.com>
2020-10-10 23:53:22 +1100

    added required changes due to python 2 format

--- base_model.py
+++ base_model.py
@@ -264,7 +264,7 @@ class BaseModel(object):
                                      str(global_step)+".npy")
 
         print("Loading the model from %s..." %save_path)
-        data_dict = np.load(save_path, encoding='latin1').item()
+        data_dict = np.load(save_path, allow_pickle=True, encoding='latin1').item()
         count = 0
         for v in tqdm(tf.global_variables()):
             if v.name in data_dict.keys():
